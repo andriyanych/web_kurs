c3b2b8dcc60bc97db1a844438c92e9e1afb27a74
usr112 <andriyanovsv@gmail.com>
2022-01-21 22:52:50 +0800

    start

---
+++ ASP.NETMyCompany/Areas/User/Controllers/HomeController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MyCompany.Domain;
+
+namespace MyCompany.Areas.User.Controllers
+{
+    [Area("User")]
+    public class HomeController : Controller
+    {
+        private readonly DataManager dataManager;
+
+        public HomeController(DataManager dataManager)
+        {
+            this.dataManager = dataManager;
+        }
+
+        public IActionResult Index()
+        {
+            return View(dataManager.TextFields.GetTextFieldByCodeWord("PageIndex"));
+        }
+
+        public IActionResult Contacts()
+        {
+            return View(dataManager.TextFields.GetTextFieldByCodeWord("PageContacts"));
+        }
+    }
+}
---
+++ ASP.NETMyCompany/Controllers/HomeController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using MyCompany.Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MyCompany.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly DataManager dataManager;
+
+        public HomeController(DataManager dataManager)
+        {
+            this.dataManager = dataManager;
+        }
+
+        public IActionResult Index()
+        {
+            return View(dataManager.TextFields.GetTextFieldByCodeWord("PageIndex"));
+        }
+    }
+}
